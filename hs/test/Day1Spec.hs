edfc9100e7cace1c206249e195a9a3504eb61aeb
Daniel Lin <ephemient@gmail.com>
2020-12-01 22:26:12 -0500

    fixup! Day 1: Report Repair

--- hs/test/Day1Spec.hs
+++ hs/test/Day1Spec.hs
@@ -6,8 +6,8 @@ import Test.Hspec (Spec, describe, it, shouldBe)
 spec :: Spec
 spec = do
     describe "part 1" $ do
-        it "examples" $
+        it "examples" $ do
             day1a (unlines ["1721", "979", "366", "299", "675", "1456"]) `shouldBe` Just 514579
     describe "part 2" $ do
-        it "examples" $
+        it "examples" $ do
             day1b (unlines ["1721", "979", "366", "299", "675", "1456"]) `shouldBe` Just 241861950
