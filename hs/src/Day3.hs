2e854ac4733fd681bc58603fd951d23955850833
Daniel Lin <ephemient@gmail.com>
2020-12-03 03:30:45 -0500

    Day 3: More speedup with Monoid Sum and UVector

--- hs/src/Day3.hs
+++ hs/src/Day3.hs
@@ -4,20 +4,26 @@ Description:    <https://adventofcode.com/2020/day/3 Day 3: Toboggan Trajectory>
 -}
 module Day3 (day3a, day3b) where
 
-import Data.List
-import Data.Ratio
-
-day3 :: [Ratio Int] -> String -> Int
-day3 slopes input = product $ length . filter id <$> transpose
-  [ [ denominator slope == 1 && line !! (numerator slope `rem` n) == '#'
-    | slope <- map (i *) slopes
-    ]
-  | (i, line) <- zip [0..] $ lines input
-  , let n = length line
-  ]
+import Data.List (foldl')
+import Data.Monoid (Sum(Sum))
+import qualified Data.Vector.Unboxed as V ((!), fromList, length)
 
 day3a :: String -> Int
-day3a = day3 [3]
+day3a = length . filter ok . zip [0, 3..] . lines
+  where ok (i, line) = line !! (i `rem` length line) == '#'
 
 day3b :: String -> Int
-day3b = day3 [1, 3, 5, 7, 1 % 2]
+day3b input = a * b * c * d * e where
+    (Sum a, Sum b, Sum c, Sum d, Sum e) = foldl' (<>) mempty
+      [ ( count i
+        , count $ 3 * i
+        , count $ 5 * i
+        , count $ 7 * i
+        , case i `quotRem` 2 of
+              (j, 0) -> count j
+              _ -> mempty
+        )
+      | (i, line) <- zip [0..] $ lines input
+      , let line' = V.fromList line
+            count j = Sum $ fromEnum $ line' V.! (j `rem` V.length line') == '#'
+      ]
