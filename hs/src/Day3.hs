cbbc1884f4a947338b18614ddf734f15d0fa6536
Daniel Lin <ephemient@gmail.com>
2020-12-03 02:02:49 -0500

    Day 3: Speed up by traversing input only once

--- hs/src/Day3.hs
+++ hs/src/Day3.hs
@@ -4,15 +4,20 @@ Description:    <https://adventofcode.com/2020/day/3 Day 3: Toboggan Trajectory>
 -}
 module Day3 (day3a, day3b) where
 
-import Data.List.Split (chunksOf)
+import Data.List
+import Data.Ratio
 
-day3 :: Int -> [[Bool]] -> Int
-day3 n = length . filter head . zipWith drop [0, n..] . map cycle
+day3 :: [Ratio Int] -> String -> Int
+day3 slopes input = product $ length . filter id <$> transpose
+  [ [ denominator slope == 1 && line !! (numerator slope `rem` n) == '#'
+    | slope <- map (i *) slopes
+    ]
+  | (i, line) <- zip [0..] $ lines input
+  , let n = length line
+  ]
 
 day3a :: String -> Int
-day3a = day3 3 . map (map (== '#')) . lines
+day3a = day3 [3]
 
 day3b :: String -> Int
-day3b input = day3 1 g * day3 3 g * day3 5 g * day3 7 g * day3 1 g' where
-    g = map (== '#') <$> lines input
-    g' = head <$> chunksOf 2 g
+day3b = day3 [1, 3, 5, 7, 1 % 2]
