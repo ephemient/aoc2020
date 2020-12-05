6e345197a4836406a7190b134ae88813793bb745
Daniel Lin <ephemient@gmail.com>
2020-12-05 11:19:38 -0500

    fixup! Day 5: Binary Boarding

---
+++ hs/test/Day5Spec.hs
@@ -0,0 +1,15 @@
+module Day5Spec (spec) where
+
+import Day5 (day5a)
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+spec :: Spec
+spec = do
+    describe "part 1" $ do
+        it "examples" $ do
+            day5a "FBFBBFFRLR" `shouldBe` Just 357
+            day5a "BFFFBBFRRR" `shouldBe` Just 567
+            day5a "FFFBBBFRRR" `shouldBe` Just 119
+            day5a "BBFFBBFRLL" `shouldBe` Just 820
+            day5a "FBFBBFFRLR\nBFFFBBFRRR\nFFFBBBFRRR\nBBFFBBFRLL" `shouldBe`
+                Just 820
