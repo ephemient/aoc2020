ec621258ff1da7d2ccc194c0ea0433e22ae560be
Daniel Lin <ephemient@gmail.com>
2020-12-05 02:12:48 -0500

    Day 5: Binary Boarding

--- rs/benches/criterion.rs
+++ rs/benches/criterion.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate build_const;
 
-use aoc2020::{day1, day2, day3, day4};
+use aoc2020::{day1, day2, day3, day4, day5};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 build_const!("aoc2020");
@@ -13,8 +13,10 @@ fn aoc2020_bench(c: &mut Criterion) {
     c.bench_function("day 2 part 2", |b| b.iter(|| day2::part2(black_box(DAY2))));
     c.bench_function("day 3 part 1", |b| b.iter(|| day3::part1(black_box(DAY3))));
     c.bench_function("day 3 part 2", |b| b.iter(|| day3::part2(black_box(DAY3))));
-    c.bench_function("day 4 part 1", |b| b.iter(|| day4::part1(black_box(DAY3))));
-    c.bench_function("day 4 part 2", |b| b.iter(|| day4::part2(black_box(DAY3))));
+    c.bench_function("day 4 part 1", |b| b.iter(|| day4::part1(black_box(DAY4))));
+    c.bench_function("day 4 part 2", |b| b.iter(|| day4::part2(black_box(DAY4))));
+    c.bench_function("day 5 part 1", |b| b.iter(|| day5::part1(black_box(DAY5))));
+    c.bench_function("day 5 part 2", |b| b.iter(|| day5::part2(black_box(DAY5))));
 }
 
 criterion_group!(aoc2020, aoc2020_bench);
---
+++ rs/src/day5.rs
@@ -0,0 +1,59 @@
+use super::util::Error;
+use itertools::Itertools;
+use std::cmp::max;
+
+fn parse(line: &str) -> Result<u32, Error> {
+    line.chars().try_fold(0, |acc, c| match c {
+        'F' | 'L' => Ok(acc << 1),
+        'B' | 'R' => Ok(acc << 1 | 1),
+        _ => Err(Error),
+    })
+}
+
+pub fn part1<'a, I, S>(lines: I) -> Result<Option<u32>, Error>
+where
+    I: IntoIterator<Item = &'a S>,
+    S: AsRef<str> + 'a,
+{
+    Ok(lines
+        .into_iter()
+        .try_fold(None, |acc, line| -> Result<_, Error> {
+            let num = parse(line.as_ref())?;
+            Ok(Some(acc.map_or(num, |acc| max(acc, num))))
+        })?)
+}
+
+pub fn part2<'a, I, S>(lines: I) -> Result<Option<u32>, Error>
+where
+    I: IntoIterator<Item = &'a S>,
+    S: AsRef<str> + 'a,
+{
+    let mut nums = lines
+        .into_iter()
+        .map(|line| parse(line.as_ref()))
+        .collect::<Result<Vec<_>, _>>()?;
+    nums.sort();
+    Ok(nums
+        .iter()
+        .tuple_windows()
+        .filter_map(|(l, r)| if *l + 1 < *r { Some(*l + 1) } else { None })
+        .next())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_examples() -> Result<(), Error> {
+        assert_eq!(Some(357), part1(&["FBFBBFFRLR"])?);
+        assert_eq!(Some(567), part1(&["BFFFBBFRRR"])?);
+        assert_eq!(Some(119), part1(&["FFFBBBFRRR"])?);
+        assert_eq!(Some(820), part1(&["BBFFBBFRLL"])?);
+        assert_eq!(
+            Some(820),
+            part1(&["FBFBBFFRLR", "BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL"])?
+        );
+        Ok(())
+    }
+}
