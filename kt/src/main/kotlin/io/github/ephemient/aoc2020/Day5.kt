93d7eb5725285e5d066d9c5295c1f1d312d781e9
Daniel Lin <ephemient@gmail.com>
2020-12-05 04:16:00 -0500

    Day 5: Save some bit operations

--- kt/src/main/kotlin/io/github/ephemient/aoc2020/Day5.kt
+++ kt/src/main/kotlin/io/github/ephemient/aoc2020/Day5.kt
@@ -2,7 +2,7 @@ package io.github.ephemient.aoc2020
 
 class Day5(lines: List<String>) {
     private val seats = lines
-        .map { it.fold(0) { acc, c -> acc shl 1 or (c.toInt() shr 2 xor 1 and 1) } }
+        .map { it.fold(0) { acc, c -> acc shl 1 or (c.toInt().inv() and 4) } shr 2 }
         .sorted()
 
     fun part1(): Int? = seats.lastOrNull()
