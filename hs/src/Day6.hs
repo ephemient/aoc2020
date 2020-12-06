df867ad8c5db1bc16c81cb5d5aceae517be01b1e
Daniel Lin <ephemient@gmail.com>
2020-12-06 01:45:19 -0500

    Day 6: Bitsets are faster than Sets

--- hs/src/Day6.hs
+++ hs/src/Day6.hs
@@ -4,14 +4,20 @@ Description:    <https://adventofcode.com/2020/day/6 Day 6: Custom Customs>
 -}
 module Day6 (day6a, day6b) where
 
-import Data.List (foldl1', nub)
-import Data.List.Split (splitOn)
-import Data.Set (empty, fromList, intersection, size)
+import Data.Bits ((.&.), (.|.), popCount, shiftL)
+import Data.Char (ord)
+import Data.List (foldl')
+
+day6 :: Int -> (Int -> Int -> Int) -> String -> Int
+day6 initial merge = uncurry add . foldl' f (0, initial) . lines where
+    add acc k = acc + popCount k
+    f (acc, k) line = case foldl' g 0 line of
+        0 -> (add acc k, initial)
+        row -> (acc, merge k row)
+    g a c = a .|. 1 `shiftL` (ord c .&. 31)
 
 day6a :: String -> Int
-day6a = sum . map (length . nub . filter (/= '\n')) . splitOn "\n\n"
+day6a = day6 0 (.|.)
 
 day6b :: String -> Int
-day6b = sum . map (size . intersects . fmap fromList . lines) . splitOn "\n\n"
-  where intersects [] = empty
-        intersects sets = foldl1' intersection sets
+day6b = day6 (-1) (.&.)
