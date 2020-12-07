5b891b7d30dbc04a3e0bdd6fde82f6c12132cba5
Daniel Lin <ephemient@gmail.com>
2020-12-07 03:28:33 -0500

    Day 7: Tying the knot

--- hs/src/Day7.hs
+++ hs/src/Day7.hs
@@ -7,26 +7,27 @@ module Day7 (day7a, day7b) where
 
 import Data.List (unfoldr)
 import Data.Map (Map)
-import qualified Data.Map as Map ((!?), filter, fromList, size)
-import Data.Maybe (fromMaybe, mapMaybe)
+import qualified Data.Map as Map (elems, findWithDefault, fromList)
+import Data.Maybe (mapMaybe)
 
 parse :: String -> Map (String, String) [(Int, (String, String))]
 parse = Map.fromList . mapMaybe (parseLine . words) . lines where
     parseLine (a:b:_:_:rest) = Just ((a, b), unfoldr parseItems rest)
     parseLine _ = Nothing
-    parseItems ((reads -> (n, ""):_):a:b:_:rest) = Just ((n :: Int, (a, b)), rest)
+    parseItems ((reads -> (n, ""):_):a:b:_:rest) = Just ((n, (a, b)), rest)
     parseItems _ = Nothing
 
 day7a :: String -> Int
-day7a input = Map.size $ Map.filter (any hasGold) bags where
+day7a input = length . filter id $ Map.elems hasGold where
     bags = parse input
-    hasGold (_, ("shiny", "gold")) = True
-    hasGold (_, item) = maybe False (any hasGold) $ bags Map.!? item
+    hasGold = any (isTransitiveGold . snd) <$> bags
+    isTransitiveGold ("shiny", "gold") = True
+    isTransitiveGold item = Map.findWithDefault False item hasGold
 
 day7b :: String -> Int
 day7b input = sum $ expand 1 ("shiny", "gold") where
     bags = parse input
     expand n item = do
-        (m', item') <- fromMaybe [] $ bags Map.!? item
+        (m', item') <- Map.findWithDefault [] item bags
         let n' = n * m'
         n' : expand n' item'
