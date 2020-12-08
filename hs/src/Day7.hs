3d8d575b016bbd0dd8883ca22ea705551993a099
Daniel Lin <ephemient@gmail.com>
2020-12-08 08:51:25 -0500

    Day 7: Reverse part 1 map for some speed

--- hs/src/Day7.hs
+++ hs/src/Day7.hs
@@ -6,27 +6,30 @@ Description:    <https://adventofcode.com/2020/day/7 Day 7: Handy Haversacks>
 module Day7 (day7a, day7b) where
 
 import Data.List (unfoldr)
-import Data.Map (Map)
-import qualified Data.Map as Map (elems, findWithDefault, fromList)
+import qualified Data.Map as Map (findWithDefault, fromListWith)
+import qualified Data.Set as Set (fromList, size)
 import Data.Maybe (mapMaybe)
 
-parse :: String -> Map (String, String) [(Int, (String, String))]
-parse = Map.fromList . mapMaybe (parseLine . words) . lines where
+parse :: String -> [((String, String), [(Int, (String, String))])]
+parse = mapMaybe (parseLine . words) . lines where
     parseLine (a:b:_:_:rest) = Just ((a, b), unfoldr parseItems rest)
     parseLine _ = Nothing
     parseItems ((reads -> (n, ""):_):a:b:_:rest) = Just ((n, (a, b)), rest)
     parseItems _ = Nothing
 
+goal :: (String, String)
+goal = ("shiny", "gold")
+
 day7a :: String -> Int
-day7a input = length . filter id $ Map.elems hasGold where
-    bags = parse input
-    hasGold = any (isTransitiveGold . snd) <$> bags
-    isTransitiveGold ("shiny", "gold") = True
-    isTransitiveGold item = Map.findWithDefault False item hasGold
+day7a input = Set.size . Set.fromList $ expand goal where
+    bags = Map.fromListWith (++)
+        [(item, [key]) | (key, items) <- parse input , (_, item) <- items]
+    expand item = items ++ concatMap expand items where
+        items = Map.findWithDefault [] item bags
 
 day7b :: String -> Int
-day7b input = sum $ expand 1 ("shiny", "gold") where
-    bags = parse input
+day7b input = sum $ expand 1 goal where
+    bags = Map.fromListWith (++) $ parse input
     expand n item = do
         (m', item') <- Map.findWithDefault [] item bags
         let n' = n * m'
