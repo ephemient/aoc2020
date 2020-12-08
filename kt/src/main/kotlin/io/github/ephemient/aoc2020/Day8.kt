cbbe153ef4941eeaad1c4872b0558d60bb06bd55
Daniel Lin <ephemient@gmail.com>
2020-12-08 09:35:30 -0500

    Day 8: Backtracking part 2

--- kt/src/main/kotlin/io/github/ephemient/aoc2020/Day8.kt
+++ kt/src/main/kotlin/io/github/ephemient/aoc2020/Day8.kt
@@ -13,18 +13,35 @@ class Day8(lines: List<String>) {
         return machine.acc
     }
 
-    fun part2(): Int? = instructions.asSequence().withIndex().mapNotNull { (i, instruction) ->
-        val flipped = when (instruction) {
-            is Machine.Instruction.Jmp -> Machine.Instruction.Nop(instruction.value)
-            is Machine.Instruction.Nop -> Machine.Instruction.Jmp(instruction.value)
-            else -> return@mapNotNull null
+    fun part2(): Int? = part2(0, 0, mutableSetOf(), false)
+
+    @Suppress("NestedBlockDepth", "ReturnCount")
+    private fun part2(acc: Int, ip: Int, seen: MutableSet<Int>, mutated: Boolean): Int? {
+        var acc = acc
+        var ip = ip
+        while (true) {
+            if (!seen.add(ip)) return null
+            when (val instruction = instructions.getOrNull(ip)) {
+                null -> return if (mutated) acc else null
+                is Machine.Instruction.Acc -> {
+                    acc += instruction.value
+                    ip++
+                }
+                is Machine.Instruction.Jmp -> {
+                    if (!mutated) {
+                        val fork = part2(acc, ip + 1, seen.toMutableSet(), true)
+                        if (fork != null) return fork
+                    }
+                    ip += instruction.value
+                }
+                is Machine.Instruction.Nop -> {
+                    if (!mutated) {
+                        val fork = part2(acc, ip + instruction.value, seen.toMutableSet(), true)
+                        if (fork != null) return fork
+                    }
+                    ip++
+                }
+            }
         }
-        val machine = Machine(instructions.toMutableList().also { it[i] = flipped })
-        val seen = mutableSetOf<Int>()
-        while (seen.add(machine.ip)) {
-            if (!machine.isRunning) return@mapNotNull machine.acc
-            machine.step()
-        }
-        null
-    }.firstOrNull()
+    }
 }
