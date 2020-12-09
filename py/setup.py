eafc646eca6178d93d801f44645864a3d4caecc6
Daniel Lin <ephemient@gmail.com>
2020-12-09 07:50:14 -0500

    PEP 518

--- py/setup.py
+++ py/setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup
 setup(
-    setup_requires=['setuptools>=30.4.0'],
+    setup_requires=['setuptools>=40.8.0', 'wheel>=0.30.0'],
     setup_cfg=True
 )
