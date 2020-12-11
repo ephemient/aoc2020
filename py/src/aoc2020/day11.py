de60ce457e848b9ac565610c9d9dbd3f2e9eac38
Daniel Lin <ephemient@gmail.com>
2020-12-11 04:47:43 -0500

    Day 11: Precompute adjacencies

--- py/src/aoc2020/day11.py
+++ py/src/aoc2020/day11.py
@@ -2,12 +2,58 @@ import fileinput
 import sys
 
 
+class Seat:
+    def __init__(self, initial_state):
+        self.state0 = initial_state
+        self.adjacencies = []
+
+    def next(self, is_odd, threshold):
+        current_state = self.state1 if is_odd else self.state0
+        count = sum(seat.state1 if is_odd else seat.state0
+                    for seat in self.adjacencies)
+        next_state = count < threshold if current_state else not count
+        if is_odd:
+            self.state0 = next_state
+        else:
+            self.state1 = next_state
+        return next_state
+
+
+def parse(lines, is_far):
+    max_length = max(map(len, lines))
+    seats = {(x, y): Seat(c == '#')
+             for y, line in enumerate(lines) for x, c in enumerate(line)
+             if c in '#L'}
+    for (x, y), seat in seats.items():
+        for dx in range(-1, 2):
+            for dy in range(-1, 2):
+                if not (dx or dy):
+                    continue
+                tx, ty = x + dx, y + dy
+                while 0 <= tx < max_length and 0 <= ty < len(lines):
+                    adjacent = seats.get((tx, ty))
+                    if adjacent:
+                        seat.adjacencies.append(adjacent)
+                        break
+                    if not is_far:
+                        break
+                    tx += dx
+                    ty += dy
+    return list(seats.values())
+
+
 def part1(lines):
     '''
     >>> part1(('L.LL.LL.LL', 'LLLLLLL.LL', 'L.L.L..L..', 'LLLL.LL.LL', 'L.LL.LL.LL', 'L.LLLLL.LL', '..L.L.....', 'LLLLLLLLLL', 'L.LLLLLL.L', 'L.LLLLL.LL'))
     37
     '''
-    return solve(lines, False, 4)
+    seats = parse(lines, False)
+    last_count, is_odd = None, False
+    while True:
+        count, is_odd = sum(seat.next(is_odd, 4) for seat in seats), not is_odd
+        if last_count == count:
+            return last_count
+        last_count = count
 
 
 def part2(lines):
@@ -15,36 +61,12 @@ def part2(lines):
     >>> part2(('L.LL.LL.LL', 'LLLLLLL.LL', 'L.L.L..L..', 'LLLL.LL.LL', 'L.LL.LL.LL', 'L.LLLLL.LL', '..L.L.....', 'LLLLLLLLLL', 'L.LLLLLL.L', 'L.LLLLL.LL'))
     26
     '''
-    return solve(lines, True, 5)
-
-
-def solve(lines, line_of_sight, threshold):
-    state = {(x, y): c == '#'
-             for y, line in enumerate(lines) for x, c in enumerate(line)
-             if c in '#L'}
-    last_count = sum(state.values())
+    seats = parse(lines, True)
+    last_count, is_odd = None, False
     while True:
-        new_state = {}
-        for (x, y), b in state.items():
-            adjacent = 0
-            for dx in range(-1, 2):
-                for dy in range(-1, 2):
-                    if not (dx or dy):
-                        continue
-                    for i in range(1, len(lines) + 1 if line_of_sight else 2):
-                        tx, ty = x + i * dx, y + i * dy
-                        if y < 0 or len(lines) <= y:
-                            break
-                        if (tx, ty) not in state:
-                            continue
-                        if state[(tx, ty)]:
-                            adjacent += 1
-                        break
-            new_state[(x, y)] = adjacent < threshold if b else adjacent == 0
-        state = new_state
-        count = sum(state.values())
+        count, is_odd = sum(seat.next(is_odd, 5) for seat in seats), not is_odd
         if last_count == count:
-            return count
+            return last_count
         last_count = count
 
 
