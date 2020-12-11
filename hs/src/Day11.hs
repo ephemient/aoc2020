c86470c1cabf0deda5768c700bbe379bfc58bdff
Daniel Lin <ephemient@gmail.com>
2020-12-11 08:44:43 -0500

    Day 11: A few percent speed improvement with unsafeIndex

--- hs/src/Day11.hs
+++ hs/src/Day11.hs
@@ -11,7 +11,7 @@ import Data.Maybe (listToMaybe, mapMaybe)
 import Data.Text (Text)
 import qualified Data.Text as T (length, lines, unpack)
 import Data.Vector.Unboxed (Vector)
-import qualified Data.Vector.Unboxed as V ((!), accum, foldl', fromList)
+import qualified Data.Vector.Unboxed as V (foldl', fromList, unsafeAccum, unsafeIndex)
 
 parse :: Bool -> Text -> ([[Int]], Vector Bool)
 parse far input = (adjs, V.fromList $ M.elems m) where
@@ -19,8 +19,7 @@ parse far input = (adjs, V.fromList $ M.elems m) where
     maxY = length rows
     maxX = maximum $ 0 : map T.length rows
     limitVision
-      | far = takeWhile $ inRange ((0, 0), (maxY - 1, maxX - 1))
-      | otherwise = take 1
+      | far = takeWhile $ inRange ((0, 0), (maxY - 1, maxX - 1)) | otherwise = take 1
     m = M.fromDistinctAscList $ do
         (y, row) <- zip [0..] rows
         (x, c) <- zip [0..] $ T.unpack row
@@ -34,9 +33,9 @@ parse far input = (adjs, V.fromList $ M.elems m) where
         limitVision $ zip [y + dy, y + 2 * dy..] [x + dx, x + 2 * dx..]
 
 step :: Int -> [[Int]] -> Vector Bool -> Vector Bool
-step d adjs m = V.accum step' m $ zip [0..] adjs where
-    step' False ixs = not $ any (m V.!) ixs
-    step' True ixs = null . drop d . filter id $ (m V.!) <$> ixs
+step d adjs m = V.unsafeAccum step' m $ zip [0..] adjs where
+    step' False ixs = not $ any (V.unsafeIndex m) ixs
+    step' True ixs = null . drop d . filter id $ V.unsafeIndex m <$> ixs
 
 count :: Vector Bool -> Int
 count = V.foldl' (flip $ bool id succ) 0
