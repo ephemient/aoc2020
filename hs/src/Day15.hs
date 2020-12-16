c795236337d6567cf12ecc33d0498f93fb305881
Daniel Lin <ephemient@gmail.com>
2020-12-16 07:07:32 -0500

    hlint

--- hs/src/Day15.hs
+++ hs/src/Day15.hs
@@ -6,7 +6,7 @@ Description:    <https://adventofcode.com/2020/day/15 Day 15: Rambunctious Recit
 module Day15 (day15) where
 
 import Common (readEntire)
-import Control.Monad (foldM)
+import Control.Monad (foldM, zipWithM_)
 import Control.Monad.ST (runST)
 import Data.Functor (($>))
 import Data.Text (Text)
@@ -21,7 +21,7 @@ day15 n input = do
         top = maximum $ n : (succ <$> nums)
     pure $ if n <= length nums then nums !! (n - 1) else runST $ do
         seen <- MV.new top
-        sequence_ $ zipWith (MV.write seen) nums [1..m - 1]
+        zipWithM_ (MV.write seen) nums [1..m - 1]
         let f x i = do
                 j <- MV.read seen x
                 let y = if j == 0 then 0 else i - j
