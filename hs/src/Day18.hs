ce7af83d5a7c1b8912a2e3e7913399c74dc8adb5
Daniel Lin <ephemient@gmail.com>
2020-12-18 06:57:25 -0500

    Day 18: Switch to Control.Monad.Combinators.Expr

--- hs/src/Day18.hs
+++ hs/src/Day18.hs
@@ -5,31 +5,28 @@ Description:    <https://adventofcode.com/2020/day/18 Day 18: Operation Order>
 {-# LANGUAGE BangPatterns, TypeFamilies #-}
 module Day18 (day18a, day18b) where
 
-import Data.List (foldl', foldl1')
+import Control.Monad.Combinators.Expr (Operator(InfixL), makeExprParser)
+import Data.List (foldl')
 import Data.Text (Text)
 import Data.Void (Void)
-import Text.Megaparsec (MonadParsec, ParseErrorBundle, Token, (<|>), between, eof, parse, sepEndBy, sepEndBy1, skipMany)
+import Text.Megaparsec (MonadParsec, ParseErrorBundle, Token, (<|>), between, eof, parse, sepEndBy, skipMany)
 import Text.Megaparsec.Char (char, newline)
 import Text.Megaparsec.Char.Lexer (decimal)
 
-lexeme :: (MonadParsec e s m, Token s ~ Char) => m a -> m a
+lexeme, parens :: (MonadParsec e s m, Token s ~ Char) => m a -> m a
 lexeme a = a <* skipMany (char ' ')
+parens = between (lexeme $ char '(') (lexeme $ char ')')
+
+addOp, mulOp :: (MonadParsec e s m, Token s ~ Char, Num a) => Operator m a
+addOp = InfixL $ (+) <$ lexeme (char '+')
+mulOp = InfixL $ (*) <$ lexeme (char '*')
 
 day18a :: Text -> Either (ParseErrorBundle Text Void) Int
 day18a = parse (parser <* eof) "" where
-    parser = foldl' (+) 0 <$> chain `sepEndBy` newline
-    chain = atom >>= expr
-    expr !lhs =
-        lexeme (char '+') *> (atom >>= expr . (+) lhs) <|>
-        lexeme (char '*') *> (atom >>= expr . (*) lhs) <|>
-        pure lhs
-    atom = lexeme decimal <|>
-        between (lexeme $ char '(') (lexeme $ char ')') chain
+    parser = foldl' (+) 0 <$> expr `sepEndBy` newline
+    expr = makeExprParser (lexeme decimal <|> parens expr) [[addOp, mulOp]]
 
 day18b :: Text -> Either (ParseErrorBundle Text Void) Int
 day18b = parse (parser <* eof) "" where
-    parser = foldl' (+) 0 <$> mulChain `sepEndBy` newline
-    mulChain = foldl1' (*) <$> addChain `sepEndBy1` lexeme (char '*')
-    addChain = foldl1' (+) <$> atom `sepEndBy1` lexeme (char '+')
-    atom = lexeme decimal <|>
-        between (lexeme $ char '(') (lexeme $ char ')') mulChain
+    parser = foldl' (+) 0 <$> expr `sepEndBy` newline
+    expr = makeExprParser (lexeme decimal <|> parens expr) [[addOp], [mulOp]]
