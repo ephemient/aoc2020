a69eeafdaa6cb1f6423f08491eed648c18179437
Daniel Lin <ephemient@gmail.com>
2020-12-23 04:11:14 -0500

    Day 23: Crab Cups

---
+++ py/src/aoc2020/day23.py
@@ -0,0 +1,63 @@
+from array import array
+import fileinput
+
+
+def step(arr, x):
+    a = arr[x]
+    b = arr[a]
+    c = arr[b]
+    y = arr[c]
+    t = x
+    while True:
+        t = t - 1 if t > 0 else len(arr) -1
+        if t not in (a, b, c):
+            break
+    u = arr[t]
+    arr[x] = y
+    arr[t] = a
+    arr[c] = u
+    return y
+
+
+def part1(lines):
+    '''
+    >>> part1(('389125467', ))
+    67384529
+    '''
+    arr = array('I', (0, ) * 9)
+    nums = [int(c) for c in lines[0] if c.isdigit()]
+    for i, x in enumerate(nums):
+        arr[x - 1] = nums[(i + 1) % len(nums)] - 1
+    x = nums[0] - 1
+    for _ in range(100):
+        x = step(arr, x)
+    x, acc = 0, 0
+    while True:
+        x = arr[x]
+        if not x:
+            return acc
+        acc = 10 * acc + x + 1
+
+
+def part2(lines):
+    '''
+    >>> part2(('389125467', ))
+    149245887792
+    '''
+    arr = array('I', range(1, 1000001))
+    nums = [int(c) for c in lines[0] if c.isdigit()]
+    for i, x in enumerate(nums):
+        arr[x - 1] = nums[i + 1] - 1 if i + 1 < len(nums) else 9
+    arr[-1] = nums[0] - 1
+    x = nums[0] - 1
+    for _ in range(10000000):
+        x = step(arr, x)
+    return (arr[0] + 1) * (arr[arr[0]] + 1)
+
+
+parts = (part1, part2)
+
+if __name__ == '__main__':
+    lines = list(fileinput.input())
+    print(part1(lines))
+    print(part2(lines))
--- py/tests/test_benchmark.py
+++ py/tests/test_benchmark.py
@@ -1,4 +1,4 @@
-from aoc2020 import day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14, day15, day16, day17, day18, day19, day20, day21, day22
+from aoc2020 import day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14, day15, day16, day17, day18, day19, day20, day21, day22, day23
 import io
 import pkg_resources
 
@@ -183,3 +183,11 @@ def test_day22_part1_bench(benchmark):
 
 def test_day22_part2_bench(benchmark):
     benchmark(day22.part2, data(22))
+
+
+def test_day23_part1_bench(benchmark):
+    benchmark(day23.part1, data(23))
+
+
+def test_day23_part2_bench(benchmark):
+    benchmark(day23.part2, data(23))
