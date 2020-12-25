83559c22ab91bdecd31811aec2e2a6491c68fb99
Daniel Lin <ephemient@gmail.com>
2020-12-25 08:13:08 -0500

    Day 25: Remove finite-typelits, perform exponentiation-by-squaring by hand (250% faster)

--- hs/src/Day25.hs
+++ hs/src/Day25.hs
@@ -2,19 +2,26 @@
 Module:         Day25
 Description:    <https://adventofcode.com/2020/day/25 Day 25: Combo Breaker>
 -}
-{-# LANGUAGE DataKinds, TypeApplications #-}
 module Day25 (day25) where
 
 import Common (readEntire)
 import Data.Either (rights)
-import Data.Finite (getFinite)
 import Data.List (elemIndex)
 import Data.Text (Text)
 import qualified Data.Text as T (words)
 import qualified Data.Text.Read as T (decimal)
 
-day25 :: Text -> Maybe Integer
+day25 :: Text -> Maybe Int
 day25 input = do
     [pub1, pub2] <- pure . rights $ readEntire T.decimal <$> T.words input
-    e <- elemIndex pub2 $ iterate (7 *) 1
-    pure . getFinite @20201227 $ pub1 ^ e
+    e <- elemIndex pub2 $ iterate ((`mod` 20201227) . (7 *)) 1
+    pure $ pub1 `powMod` e
+
+powMod :: (Integral b, Integral e) => b -> e -> b
+powMod _ 0 = 1
+powMod b e = powMod' 1 b e where
+    powMod' k _ 0 = k
+    powMod' k b' e' = powMod' k' b'' e'' where
+        (e'', r) = e' `divMod` 2
+        k' = if r == 0 then k else k * b' `mod` 20201227
+        b'' = b' * b' `mod` 20201227
